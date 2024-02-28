b3eca28b6eb1d15fdae831531069130a2050e7e2
Régis Thiéry <regthiery@gmail.com>
2024-02-28 21:19:57 +0100

    README.md and options implementation

--- README.md
+++ README.md
@@ -1,16 +1,45 @@
 # AtmoTerre
 
-Code de calcul de spéciation d'une atmosphère en équilibre avec un océan magmatique
+Ce code permet de calculer la spéciation d'une atmosphère en équilibre avec un océan magmatique.
 
 ## Utilisation
 
-python3 main1
+Pour utiliser ce code, exécutez le script `main1.py` avec l'une des commandes suivantes :
 
+```bash
+python3 main1.py fO2
 
-## Auteurs
+python3 main1.py speciation
+
+# AtmoTerre
+
+Ce code permet de calculer la spéciation d'une atmosphère en équilibre avec un océan magmatique.
+
+## Utilisation
+
+Pour utiliser ce code, exécutez le script `main1.py` avec l'une des commandes suivantes :
+
+python3 main1.py fO2
+Calcule la fugacité d'oxygène du magma.
+
+python3 main1.py speciation
+Calcule la spéciation de l'atmosphère.
+
+python3 main1.py curve
+Calcule une courbe de spéciation.
+
+### Options
+
+Vous pouvez également spécifier les options suivantes :
+
+--t: Fixe la température en Kelvin.
+--p: Fixe la pression en bar.
+--ho: Fixe le rapport atomique H/O.
+
+## Auteur
 
 - Régis THIERY
 
 ## Contact
 
-regthiery@gmail.com
+Pour toute question ou suggestion, veuillez contacter Régis THIERY à l'adresse e-mail suivante : regthiery@gmail.com
--- main1.py
+++ main1.py
@@ -1,48 +1,76 @@
 import numpy as np
-from scipy.optimize import fsolve
-from scipy.optimize import newton
 
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import argparse
+
 
 from src.thermodynamicData import ThermodynamicData
 from src.atmoSpec import Atmosphere
 
-# data = ThermodynamicData()
 
-atmosphere = Atmosphere()
-atmosphere.problem = 21
-atmosphere.algorithm = 1
-T = 2000
-P = 100
-hc = 0
-ho = 4
-hn = 0
+if __name__ == "__main__":
+
+     parser = argparse.ArgumentParser(prog="atmoter", description="Spéciation atmosphère")
+     parser.add_argument('cmd', help='Commande')
+     parser.add_argument('--ho'  , type=float, help='Valeur du rapport H/O')
+     parser.add_argument('--hc'  , type=float, help='Valeur du rapport H/C')
+     parser.add_argument('--hn'  , type=float, help='Valeur du rapport H/N')
+     parser.add_argument('--t'   , type=float, help='Valeur de la température T en Kelvin')
+     parser.add_argument('--p'   , type=float, help='Valeur de la pression P en bar')
+     parser.add_argument('--algo', type=int, choices=[1, 2], help='Algorithme (1 ou 2)')
+
+     args = parser.parse_args()
+     
+     print ("Atmoter")
+
+     atmosphere = Atmosphere()
+     atmosphere.problem = 21
+
+     T  = args.t  if args.t  is not None else 2000
+     P  = args.p  if args.p  is not None else 100
+     ho = args.ho if args.ho is not None else 4
+     hc = args.ho if args.hc is not None else 0
+     hn = args.ho if args.hn is not None else 0
+
+     atmosphere.algorithm = args.algo if args.algo is not None else 1 
+     
+     if args.cmd == 'fO2':
+          atmosphere.problem = 30
+     elif args.cmd == 'speciation':
+          atmosphere.problem = 20
+     elif args.cmd == 'curve':
+          atmosphere.problem = 21     
 
          # Calcul d'un point de spéciation pour une valeur H/O donnée
 
-if atmosphere.problem == 20:
-    if atmosphere.algorithm == 2:
-         atmosphere.calculateMolesNumber(T, P, hc, ho, hn)
-         atmosphere.calculateReactionConstants()
-         initials = [0.501, 1e-10]
-         atmosphere.calculateSpeciation(initials)
-    elif atmosphere.algorithm == 1:     
-         atmosphere.calculateMolesNumber(T, P, hc, ho, hn)
-         atmosphere.calculateReactionConstants()
-         atmosphere.calculateSpeciation(None)
+     if atmosphere.problem == 20:
+          if atmosphere.algorithm == 2:
+              atmosphere.calculateMolesNumber(T, P, hc, ho, hn)
+              atmosphere.calculateReactionConstants()
+              initials = [0.501, 1e-10]
+              atmosphere.calculateSpeciation(initials)
+          elif atmosphere.algorithm == 1:     
+              atmosphere.calculateMolesNumber(T, P, hc, ho, hn)
+              atmosphere.calculateReactionConstants()
+              atmosphere.calculateSpeciation(None)
          
          # Calcul de la courbe de speciation
-elif atmosphere.problem == 21:
-    if atmosphere.algorithm == 2:
-         atmosphere.calculateMolesNumber(T, P, hc, ho, hn)
-         atmosphere.calculateReactionConstants()
-         initials = [0.1, 0.8]
-         atmosphere.calculateSpeciationCurve(1e-9, 40, initials)
-    elif atmosphere.algorithm == 1:     
-         atmosphere.calculateMolesNumber(T, P, hc, ho, hn)
-         atmosphere.calculateReactionConstants()
-         atmosphere.calculateSpeciation(None)
-         atmosphere.calculateSpeciationCurve(1e-9, 40, None)
+     elif atmosphere.problem == 21:
+          if atmosphere.algorithm == 2:
+              atmosphere.calculateMolesNumber(T, P, hc, ho, hn)
+              atmosphere.calculateReactionConstants()
+              initials = [0.1, 0.8]
+              atmosphere.calculateSpeciationCurve(1e-9, 40, initials)
+          elif atmosphere.algorithm == 1:     
+              atmosphere.calculateMolesNumber(T, P, hc, ho, hn)
+              atmosphere.calculateReactionConstants()
+              atmosphere.calculateSpeciation(None)
+              atmosphere.calculateSpeciationCurve(1e-9, 40, None)
     
 
+         # Calcul de la fugacité d'O2 (tampon IW)
+     elif atmosphere.problem == 30:
+         atmosphere.calculateMolesNumber(T, P, hc, ho, hn)
+         atmosphere.calculateReactionConstants()
+         print (f"T              (K)  : {atmosphere.T:10.4g}")
+         print (f"Fugacité d'O2  (bar): {atmosphere.fO2:10.4g}")
+    
